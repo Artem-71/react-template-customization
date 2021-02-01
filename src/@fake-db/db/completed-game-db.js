3f5948e1f849a4458e38efef03cbf7dddd11070b
Mango-jam-0513 <mangojamstar0513@outlook.com>
2021-02-01 18:29:59 +0800

    update

--- src/@fake-db/db/completed-game-db.js
+++ src/@fake-db/db/completed-game-db.js
@@ -135,7 +135,7 @@ const completedGamesDB = {
 		{
 			id: '5725a680ae1ae9a3c960d487',
 			name: 'Henderson',
-			lastName: 'Cambiason',
+			lastName: 'Cambia',
 			avatar: 'assets/images/avatars/Henderson.jpg',
 			textChat: '10',
 			voiceChat: '200',
