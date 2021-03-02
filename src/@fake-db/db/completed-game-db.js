28bf7e181e7287174cda029e62a5a4d8af08a85a
Mango-jam-0513 <mangojamstar0513@outlook.com>
2021-03-02 10:25:33 +0200

    database updated

--- src/@fake-db/db/completed-game-db.js
+++ src/@fake-db/db/completed-game-db.js
@@ -147,7 +147,7 @@ const completedGamesDB = {
 		},
 		{
 			id: '5725a680b8d240c011dd224b',
-			name: 'Jose Fan',
+			name: 'Jose',
 			lastName: 'Lakefield',
 			avatar: 'assets/images/avatars/Josefina.jpg',
 			textChat: '10',
