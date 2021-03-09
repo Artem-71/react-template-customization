5e44fb9907e62465da5efb2ad88a9200494f55e3
Mango-jam-0513 <mangojamstar0513@outlook.com>
2021-03-09 10:40:24 +0200

    database updated

--- src/@fake-db/db/completed-game-db.js
+++ src/@fake-db/db/completed-game-db.js
@@ -173,7 +173,7 @@ const completedGamesDB = {
 		},
 		{
 			id: '5725a6801146cce777df2a08',
-			name: 'Vitalii',
+			name: 'Vitaly',
 			lastName: 'Peasegood',
 			avatar: 'assets/images/avatars/Lily.jpg',
 			textChat: '10',
