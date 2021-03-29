d72f3eb8bfa79d63a4cf5c856c097c1871dd1d14
Mango-jam-0513 <mangojamstar0513@outlook.com>
2021-03-29 06:13:40 +0300

    database upated

--- src/@fake-db/db/completed-game-db.js
+++ src/@fake-db/db/completed-game-db.js
@@ -18,7 +18,7 @@ const completedGamesDB = {
 		{
 			id: '5725a680606588342058356d',
 			name: 'Arnoldun',
-			lastName: 'Matlock Tom',
+			lastName: 'Matlock',
 			avatar: 'assets/images/avatars/Arnold.jpg',
 			textChat: '100',
 			voiceChat: '300',
