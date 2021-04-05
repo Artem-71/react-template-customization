bdc5e35b038f52053649d8a7e939a6c998a29a5f
Mango-jam-0513 <mangojamstar0513@outlook.com>
2021-04-05 05:12:12 +0300

    database updated

--- src/@fake-db/db/completed-game-db.js
+++ src/@fake-db/db/completed-game-db.js
@@ -43,7 +43,7 @@ const completedGamesDB = {
 		},
 		{
 			id: '5725a6809fdd915739187ed5',
-			name: 'Blair Hodem',
+			name: 'Hodem',
 			lastName: 'Strangeway',
 			avatar: 'assets/images/avatars/Blair.jpg',
 			textChat: '33',
