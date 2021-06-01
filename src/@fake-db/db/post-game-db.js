9e213643355745901bffc44753322380b9acb33f
Mango-jam-0513 <mangojamstar0513@outlook.com>
2021-06-01 17:04:09 +0800

    database updated

--- src/@fake-db/db/post-game-db.js
+++ src/@fake-db/db/post-game-db.js
@@ -17,7 +17,7 @@ const postGameDB = {
         },
         {
             id: 1,
-			groupName: 'Best bank group',
+			groupName: 'Bank group',
 			dateTime: setDate(2020, 12, 0, 0, 0, 0),
 			duration: 30,
 			status: 'complete',
